a75fdcd398433f6f669558d5c911d80a806723d0
iiriis <55133414+iiriis@users.noreply.github.com>
2021-09-19 18:24:35 +0530

    Update Firefighting_Bot.ino

--- Firefighting_Bot.ino
+++ Firefighting_Bot.ino
@@ -7,6 +7,7 @@ pinMode(5, OUTPUT);
 
 pinMode(A0, INPUT_PULLUP);
 }
+
 float x=0;
 
 void loop()
@@ -40,3 +41,33 @@ analogWrite(5, i);
 }
 
 }
+
+int x[4], y[4];
+int n=4;
+
+void centroid()
+{
+float a, cx, cy, t;
+int i, i1;
+
+a = 0.0;
+i1 = 1;
+for (i=0; i<n; i++) {
+a += x[i] * y[i1] - x[i1] * y[i];
+i1 = (i1 + 1) % n;
+}
+a *= 0.5;
+cx = cy = 0.0;
+i1 = 1;
+for (i=0; i<n; i++) 
+{
+t = x[i]*y[i1] - x[i1]*y[i];
+cx += (x[i]+x[i1]) * t;
+cy += (y[i]+y[i1]) * t;
+i1 = (i1 + 1) % n;
+}
+cx = cx / (6.0 * a);
+cy = cy / (6.0 * a);
+}
+
+
