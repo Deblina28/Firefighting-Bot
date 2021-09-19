3cba3b02335cb7eaea0b6d9f0094c946f1bc0efa
Deblina Chattopadhyay <62389473+Deblina28@users.noreply.github.com>
2021-09-19 15:58:14 +0530

    Attached alarming feature

--- Firefighting_Bot.ino
+++ Firefighting_Bot.ino
@@ -7,7 +7,28 @@ float x=0;
 
 void loop()
 {
-x=0.5*analogRead(A0) + 0.5*x;
+x=0.5analogRead(A0) + 0.5x;
 Serial.println(x);
 }
+
+if(x<300)
+alarm();
+
+
+}
+
+void alarm()
+{
+for(int i=0;i<256;i++, delay(4))
+{
+analogWrite(3, i);
+analogWrite(5, i);
+}
+
+for(int i=255;i>0;i--, delay(4))
+{
+analogWrite(3, i);
+analogWrite(5, i);
+}
+
 }
